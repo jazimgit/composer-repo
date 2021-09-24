c9d02a29cdbea24ed3af51081870e0451c7a45fc
jazimgit <git@sahil.com>
2021-09-25 02:48:43 +0530

    added code

--- pipeline/helper_script/py_validater.sh
+++ pipeline/helper_script/py_validater.sh
@@ -5,14 +5,14 @@ cd ../../..
 
 cd composer-repo
 
-mkdir tmp
-touch tmp/change_py_files.txt
+touch change_py_files.txt
 
-git diff-tree --name-status --no-commit-id -r 922fb6b15f7e1a9672d7b99e08815ab60117819a a2632c6a0a0a5a3c8580c7a423999dd27400b0f5 | grep -wv ^D | awk '{print $2}' >> tmp/change_py_files.txt
+git diff-tree --name-status --no-commit-id -r 922fb6b15f7e1a9672d7b99e08815ab60117819a a2632c6a0a0a5a3c8580c7a423999dd27400b0f5 | grep -wv ^D | awk '{print $2}' >> change_py_files.txt
 
-cd tmp
 lines=$(wc -l change_py_files.txt)
+echo "####################################"
 echo "line count is $lines"
+echo "####################################"
 if [ "$lines" != 0 ];
 then
      echo "there is change files"
